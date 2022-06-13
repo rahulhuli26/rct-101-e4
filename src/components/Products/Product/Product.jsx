c4dcbb50cbef7f1a3c2d0d242b53adc2d14c03b7
rahulhuli26 <rahulhulikoppe38@gmail.com>
2022-06-13 18:48:53 +0530

    done

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -7,40 +7,44 @@ import React,{useState} from "react";
 //   removeItemFromCart,
 //   updateCartItem,
 // } from "../../../store/cart/cart.actions";
+
 const Product = () => {
   const [product, setProduct] = useState([]);
-  React.useEffect(() => {
+    React.useEffect(() => {
     showData()
   }, [])
+
   const showData = () => {
-    axios.get(`http://localhost:8080/products`).then((res) => {
+    axios.get(` http://localhost:8080/products`).then((res) => {
       setProduct(res.data);
+     
       console.log(res.data);
     })
   }
 
 
-  const id = null;
+   const id = null;
   return (
-    <div>
-      <div>
-        {product.map((product) => (<Product key={product.id} {...product} />))}
-      </div>
-    <div  data-cy={`product-${id}`}>
-      <h3 data-cy="product-name">name</h3>
-      <h6 data-cy="product-description">description</h6>
-      <button data-cy="product-add-item-to-cart-button"></button>
-      <div>
-        <button data-cy="product-increment-cart-item-count-button"></button>
-        <span data-cy="product-count"></span>
-        <button data-cy="product-decrement-cart-item-count-button"></button>
-        <button data-cy="product-remove-cart-item-button"></button>
-      </div>
-    </div>
     
-    </div>
-  );
-};
+    <div>
+       <div>
+         {product.map((product) => 
+          <div  data-cy={`product-${id}`}>
+          <h3 data-cy="product-name">{product.name}</h3>
+          <h6 data-cy="product-description">{product.description}</h6>
+          <button data-cy="product-add-item-to-cart-button"></button>
+          <div>
+            <button data-cy="product-increment-cart-item-count-button"></button>
+            <span data-cy="product-count"></span>
+            <button data-cy="product-decrement-cart-item-count-button"></button>
+            <button data-cy="product-remove-cart-item-button"></button>
+          </div>
+        </div>
+         )}
+      </div>
+   </div>
+   );
+}
 
 
 export default Product;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {BrowserRouter} from "react-router-dom";
-import { AuthContext } from "./components/Context/AuthContext";
+import { AuthProvider } from "./components/Context/AuthContext";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-        <AuthContext>
+        <AuthProvider>
             <App />
-        </AuthContext>
+        </AuthProvider>
         </BrowserRouter>
   </React.StrictMode>
 );
